2d02042ca70a7f2acc3e383d0eba94e267bb4836
naranjo24 <bsnaranjo222@misena.edu.co>
2022-06-22 06:58:46 -0500

    add class

---
+++ clase/Migrations/20220616003418_addcity.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace clase.Migrations
+{
+    public partial class addcity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ clase/Migrations/20220622025437_addclass.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace clase.Migrations
+{
+    public partial class addclass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
